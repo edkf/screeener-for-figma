2b05ca3b01f0838bf3e9cfa906226704be225ed5
Edgard Kozlowski <edgardfernandes11@gmail.com>
2020-04-30 22:55:31 -0300

    Creating mockups

--- code.ts
+++ code.ts
@@ -1,4 +1,5 @@
 figma.showUI(__html__)
+figma.ui.resize(400, 200)
 
 function clone(val) {
   return JSON.parse(JSON.stringify(val))
@@ -9,21 +10,29 @@ figma.ui.onmessage = msg => {
 
     const selection = figma.currentPage.selection
 
-    selection.forEach(element => {
+    selection.forEach((element, index) => {
 
       // SLIDE PROPRERTIES
       const slide = figma.createFrame()
-      slide.x = element.x
+      slide.x = (1920 + 200) * index
       slide.y = element.y + 1000
       slide.resize(1920, 1080)
       const fills = clone(slide.fills)
-      fills[0].color = {r: 0, g: 0, b: 0}
+      const mockupImg = figma.createImage(msg.mockupData.onePhone).hash
+
+      console.log(mockupImg)
+      
+      fills[0] = {
+        type: 'IMAGE',
+        imageHash: mockupImg,
+        scaleMode: 'FILL'
+      }
       figma.currentPage.appendChild(slide)
       slide.fills = fills
 
       const mockup = figma.createRectangle()
-      mockup.x = 0
-      mockup.y = 0
+      mockup.x = 713
+      mockup.y = 101
       mockup.resize(494, 878)
       const mockupFills = clone(slide.fills)
 
@@ -31,13 +40,18 @@ figma.ui.onmessage = msg => {
       slide.appendChild(mockup)
 
       // EXPORTING SELECTION
-      const exportedImage = element.exportAsync()
-
-      console.log(exportedImage)
+      const exportedImage = element.exportAsync({
+        format: 'PNG',
+        constraint: {
+          type: 'SCALE',
+          value: 2
+        }
+      })
 
       exportedImage
         .then(result => {
           const img = figma.createImage(result)
+          // console.log(img.hash)
           mockupFills[0] = {
             type: 'IMAGE',
             imageHash: img.hash,
@@ -69,5 +83,5 @@ figma.ui.onmessage = msg => {
 
   // Make sure to close the plugin when you're done. Otherwise the plugin will
   // keep running, which shows the cancel button at the bottom of the screen.
-  // figma.closePlugin()
+  figma.closePlugin()
 }
